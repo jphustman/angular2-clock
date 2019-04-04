e4a594fc138a5fc53058ef75d725680135f91b5e
jphustman <jphustman>
2019-04-04 11:16:27 -0800

    kind of working

--- component.ts
+++ component.ts
@@ -1,35 +1,40 @@
-//our root app component
+// our root app component
 import {Component, Attribute} from '@angular/core'
 
 @Component({
-  selector: 'clock',
-  templateUrl: 'src/clock/view.html'
+  selector: 'app-clock',
+  templateUrl: './view.html'
 })
 export class ClockComponent {
     private data;
-    private format; 
-    private timer;
+    private format;
+    private timer = true;
     private intervalSet = false;
-    constructor(@Attribute("format") format, @Attribute("data") data, @Attribute('timer') timer) { 
-        var date, miliseconds;
-        this.format = format || 'h:mm:s'; 
+    constructor(@Attribute('format') format, @Attribute('data') data, @Attribute('timer') timer) {
+        let date;
+        let milliseconds = 1000;
+        this.format = format || 'h:mm:s';
         this.data = data || new Date();
-        if(timer) {
-             if(typeof this.data !== 'Date') {
-                date = new Date();
-            } else {
-                date = this.data;
-            }
+        if (this.timer) {
 
-            miliseconds = (60 - date.getSeconds()) * 1000;
-            window.setTimeout(()=>{this.refreshTime();}, miliseconds);            
+          if(typeof this.data !== 'Date') {
+          // if(typeof this.data !== 'Date') {
+            //if(typeof this.data !== 'Date') {
+            date = new Date();
+          } else {
+            date = this.data;
+          }
+
+          // milliseconds = (60 - date.getSeconds()) * 1000;
+          milliseconds = 1000;
+          window.setTimeout(() => {this.refreshTime(); }, milliseconds);
         }
-    } 
+    }
 
     refreshTime() {
         this.data = new Date();
-        if(!this.intervalSet) {
-            window.setInterval(()=>{this.refreshTime()}, 60000);
+        if (!this.intervalSet) {
+            window.setInterval(() => {this.refreshTime(); }, 1000);
             this.intervalSet = true;
         }
     }
--- module.ts
+++ module.ts
@@ -1,6 +1,6 @@
-import { NgModule }              from '@angular/core';
-import { CommonModule }          from '@angular/common';
-import { ClockComponent }  from './component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClockComponent } from './component';
 
 @NgModule({
   imports: [ CommonModule ],
